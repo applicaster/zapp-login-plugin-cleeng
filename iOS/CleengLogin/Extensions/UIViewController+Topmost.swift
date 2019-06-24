2e255d99d56eec7aa66d68865cf4c72c51f9702b
Roman Karpievich <karpievich@scand.com>
2019-06-24 15:56:21 +0300

    [IOS] Add parsing for key data from DSP.

---
+++ iOS/CleengLogin/Extensions/UIViewController+Topmost.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewController+Topmost.swift
+//  CleengLogin
+//
+//  Created by Roman Karpievich on 6/21/19.
+//
+
+import UIKit
+
+extension UIViewController {
+    class func topmostViewController() -> UIViewController? {
+        guard let window = UIApplication.shared.delegate?.window ,
+            let rootViewController = window?.rootViewController else {
+            return nil
+        }
+        
+        var top: UIViewController = rootViewController
+        while let newTop = top.presentedViewController {
+            top = newTop
+        }
+        
+        return top
+    }
+}
